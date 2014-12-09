be5945fdcf9c01fa7bbebeb3527f3402158ce5f2
Ayyappan Madhavan <ayyappan.vsm@gmail.com>
2014-12-09 17:32:55 +0530

    Rearrange files to appropriate folders

--- Euler/AnagramSolver.java
+++
@@ -1,8 +0,0 @@
-public class AnagramSolver
-{
-	public static void main(String args[])
-	{
-		String inStr = "A ILL SCREW";
-		
-	}
-}
--- Euler/Equations.java
+++
@@ -1,7 +0,0 @@
-public class Equations
-{
-	public static void main(String args[])
-	{
-		
-	}
-}
